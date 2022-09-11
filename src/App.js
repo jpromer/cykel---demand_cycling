a0a88cdfa51df244159f0789445af9cfc0bd8086
Juan Pablo Romero Laverde <112736976+jpromer@users.noreply.github.com>
2022-09-11 14:42:48 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.scss';
+import RouterApp from './routes';
+
+
+function App() {
+  return (
+    <div className="App">
+         <RouterApp />
+    </div>
+  );
+}
+
+export default App;
